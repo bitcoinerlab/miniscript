7ba87a8fab291b728708d52bd8e0260d6995f6d2
José Luis Landabaso Díaz <landabaso@gmail.com>
2022-12-29 17:30:30 +0100

    Use terser to generate slightly less bulky packages

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,7 @@
 import commonjs from 'rollup-plugin-commonjs';
+import terser from '@rollup/plugin-terser';
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 export default {
   input: './src/index.js',
@@ -6,5 +9,5 @@ export default {
     file: './dist/index.js',
     format: 'cjs'
   },
-  plugins: [commonjs()]
+  plugins: [commonjs(), isProduction && terser()]
 };
