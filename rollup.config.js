9d545a677b92c745ba0c7452ca8349fa3472f6e7
José Luis Landabaso Díaz <landabaso@gmail.com>
2023-01-25 12:16:21 +0100

    Remove the terser from production builds. Makes it easier to debug. The final user can further apply a terser it if desired

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,4 @@
 import commonjs from 'rollup-plugin-commonjs';
-import terser from '@rollup/plugin-terser';
-
-const isProduction = process.env.NODE_ENV === 'production';
 
 export default {
   input: './src/index.js',
@@ -9,5 +6,5 @@ export default {
     file: './dist/index.js',
     format: 'cjs'
   },
-  plugins: [commonjs(), isProduction && terser()]
+  plugins: [commonjs()]
 };
