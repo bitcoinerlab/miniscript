db934027370553a68e5125e13a84963dbf108266
landabaso <landabaso@gmail.com>
2026-01-27 11:23:42 +0100

    refactor: port to typescript

--- src/miniscript/types/index.ts
+++ src/miniscript/types/index.ts
@@ -1,16 +1,22 @@
 // Copyright (c) 2026 Jose-Luis Landabaso - https://bitcoinerlab.com
 // Distributed under the MIT software license
 
-export * from './correctness.js';
-export * from './malleability.js';
+export * from './correctness';
+export * from './malleability';
 
-/**
- * Combine correctness and malleability into a single type record.
- * @param {Correctness} correctness
- * @param {Malleability} malleability
- * @returns {{correctness: Correctness, malleability: Malleability}}
- */
-export const makeType = (correctness, malleability) => ({
-  correctness,
-  malleability
-});
+import type { Correctness } from './correctness';
+import type { Malleability } from './malleability';
+
+/** Combined correctness and malleability record. */
+export type MiniscriptType = {
+  correctness: Correctness;
+  malleability: Malleability;
+};
+
+/** Combine correctness and malleability into a single type record. */
+export const makeType = (
+  /** Correctness flags to combine. */
+  correctness: Correctness,
+  /** Malleability flags to combine. */
+  malleability: Malleability
+): MiniscriptType => ({ correctness, malleability });
