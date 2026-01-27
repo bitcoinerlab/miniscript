131bd48566ee28cbe38961d9b384db81418b50b7
landabaso <landabaso@gmail.com>
2026-01-27 17:33:08 +0100

    chore: use bitcoinerlab/config utils for common typescript rules, linting, code formatting and bump version to 2.0

--- test/maxLock.test.ts
+++ test/maxLock.test.ts
@@ -2,7 +2,7 @@
 // Distributed under the MIT software license
 
 import bip68 from 'bip68';
-import { maxLock, ABSOLUTE, RELATIVE } from '../src/satisfier/maxLock.js';
+import { maxLock } from '../src/satisfier/maxLock';
 describe('maxLock', () => {
   test('throws an error if lockType is undefined', () => {
     expect(() => maxLock(1, 2)).toThrow('lockType must be specified');
@@ -10,32 +10,33 @@ describe('maxLock', () => {
 
   test('should throw an error if lockType is not ABSOLUTE or RELATIVE', () => {
     expect(() => {
+      // @ts-expect-error Invalid lock type for test coverage
       maxLock(1, 2, 'INVALID');
     }).toThrow('lockType must be either "ABSOLUTE" or "RELATIVE"');
   });
 
   test('should throw an error if a and b are not integers', () => {
     expect(() => {
-      maxLock('a', 2, ABSOLUTE);
+      maxLock('a', 2, 'ABSOLUTE');
     }).toThrow('nSequence/nLockTime must be an integer: a');
     expect(() => {
-      maxLock(1.1, 1, ABSOLUTE);
+      maxLock(1.1, 1, 'ABSOLUTE');
     }).toThrow('nSequence/nLockTime must be an integer: 1.1');
   });
 
   test('returns undefined if a and b are undefined', () => {
-    expect(maxLock(undefined, undefined, ABSOLUTE)).toBe(undefined);
-    expect(maxLock(undefined, undefined, RELATIVE)).toBe(undefined);
+    expect(maxLock(undefined, undefined, 'ABSOLUTE')).toBe(undefined);
+    expect(maxLock(undefined, undefined, 'RELATIVE')).toBe(undefined);
   });
 
   test('should throw an error if nLockTime values are not both below 500000000 or both above or equal 500000000', () => {
     expect(() => {
-      maxLock(1, 500000000, ABSOLUTE);
+      maxLock(1, 500000000, 'ABSOLUTE');
     }).toThrow(
       'nLockTime values must be either below 500000000 or both above or equal 500000000'
     );
     expect(() => {
-      maxLock(500000000, 1, ABSOLUTE);
+      maxLock(500000000, 1, 'ABSOLUTE');
     }).toThrow(
       'nLockTime values must be either below 500000000 or both above or equal 500000000'
     );
@@ -46,38 +47,46 @@ describe('maxLock', () => {
       maxLock(
         bip68.encode({ seconds: 1 * 512 }),
         bip68.encode({ blocks: 2 }),
-        RELATIVE
+        'RELATIVE'
       );
     }).toThrow('a and b must both be either represent seconds or block height');
   });
 
   test('should return the maximum value of a and b if both are specified', () => {
-    expect(maxLock(1, 2, ABSOLUTE)).toBe(2);
-    expect(maxLock(2, 1, ABSOLUTE)).toBe(2);
+    expect(maxLock(1, 2, 'ABSOLUTE')).toBe(2);
+    expect(maxLock(2, 1, 'ABSOLUTE')).toBe(2);
     expect(
       maxLock(
         bip68.encode({ seconds: 1 * 512 }),
         bip68.encode({ seconds: 2 * 512 }),
-        RELATIVE
+        'RELATIVE'
       )
     ).toBe(bip68.encode({ seconds: 2 * 512 }));
     expect(
       maxLock(
         bip68.encode({ seconds: 2 * 512 }),
         bip68.encode({ seconds: 1 * 512 }),
-        RELATIVE
+        'RELATIVE'
       )
     ).toBe(bip68.encode({ seconds: 2 * 512 }));
   });
 
   test('should return the value of a if b is not specified', () => {
-    expect(maxLock(1, undefined, ABSOLUTE)).toBe(1);
-    expect(maxLock(undefined, 1, ABSOLUTE)).toBe(1);
+    expect(maxLock(1, undefined, 'ABSOLUTE')).toBe(1);
+    expect(maxLock(undefined, 1, 'ABSOLUTE')).toBe(1);
     expect(
-      maxLock(bip68.encode({ seconds: 1 * 512 }), undefined, RELATIVE)
+      maxLock(
+        bip68.encode({ seconds: 1 * 512 }),
+        undefined,
+        'RELATIVE'
+      )
     ).toBe(bip68.encode({ seconds: 1 * 512 }));
     expect(
-      maxLock(undefined, bip68.encode({ seconds: 1 * 512 }), RELATIVE)
+      maxLock(
+        undefined,
+        bip68.encode({ seconds: 1 * 512 }),
+        'RELATIVE'
+      )
     ).toBe(bip68.encode({ seconds: 1 * 512 }));
   });
 });
